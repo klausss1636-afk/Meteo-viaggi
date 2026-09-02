4754bcdbfa72c0def08bcb98a3b611085d8dd19c
klausss1636-afk <klausss1636@gmail.com>
2026-09-02 07:14:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE="meteo-viaggio-v1";
+const CACHE="meteo-viaggio-v2-ricalcolo-nebbia";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener("fetch",e=>{
   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
