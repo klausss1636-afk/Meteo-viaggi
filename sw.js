4c01647b0ffd6dd17511d38a2f266a459c60a20a
klausss1636-afk <klausss1636@gmail.com>
2026-09-10 09:27:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const CACHE="meteo-viaggio-v2-ricalcolo-nebbia";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+const CACHE='meteo-viaggio-v11-2-poi';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
 ));
-self.addEventListener("fetch",e=>{
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}));
+    return;
+  }
   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
