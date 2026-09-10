134c6abeec283fdf84226e8c2104ff898e854422
klausss1636-afk <klausss1636@gmail.com>
2026-09-10 09:34:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meteo-viaggio-v11-2-poi';
+const CACHE='meteo-viaggio-v11-3-preferiti';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
